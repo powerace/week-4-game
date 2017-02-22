e21ba105123e3a53f6f9832b518fcb2b8dbc46eb
Taniesha Robinson <taniesharobinson@mcs-macbook06.att.net>
2017-02-22 00:43:03 -0600

    base html and game object complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+var game = {
+	reginaGeorge : {
+		healthPoints : 150,
+		attackPower : 5,
+		counterAttackPower : 10
+	},
+	kathrynMerteuil : {
+		healthPoints : 60,
+		attackPower : 8,
+		counterAttackPower : 20
+	},
+	taylorVaughn : {
+		healthPoints : 90,
+		attackPower : 6,
+		counterAttackPower : 13
+	}, 
+	sharpayEvans : {
+		healthPoints : 170,
+		attackPower : 4,
+		counterAttackPower : 7
+	}
+}
