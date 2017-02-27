a52808a577e205cd249188851370c2326f62e547
Taniesha Robinson <taniesharobinson@mcs-macbook06.att.net>
2017-02-26 22:53:07 -0600

    broke down attack into separate functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,52 @@
 var game = {
 	stage : "choose character",
-	clickCounter: 0,
+	clickCounter: 1,
 	taylor : {
 		healthPoints : 160,
-		origAttackPower : 8,
 		attackPower : 8,
 		counterAttackPower : 15,
 		fullName: "Taylor Vaughan"
 	},
 	regina: {
 		healthPoints : 150,
-		origAttackPower : 6,
 		attackPower : 6,
 		counterAttackPower : 10,
 		fullName: "Regina George"
 	},
 	sharpay : {
 		healthPoints : 140,
-		origAttackPower : 4,
 		attackPower : 4,
 		counterAttackPower : 7,
 		fullName : "Sharpay Evans"
 	}, 
 	kathryn : {
 		healthPoints : 240,
-		origAttackPower : 10,
 		attackPower : 10,
 		counterAttackPower : 45,
 		fullName: "Kathryn Merteuil"
 	},
 	chooseCharacter : function(){
-		var self = this;
-		$(document).on('click', '.character', function(){
-			if (self.stage === "choose character"){
-				console.log(self.stage);
+		var outerScope = this;
+		$('.character').on('click', function(){
+			if (outerScope.stage === "choose character"){
 				$(this).addClass('you');
 				$(this).siblings('.character').appendTo('#enemies').addClass('enemies');
-				self.stage = "choose opponent";
-				console.log(self.stage);
-				//suggestion from Brian 
+				outerScope.stage = "choose opponent";
+				window.selectedCharacter = $(this).attr('data-name');
+				console.log(selectedCharacter);
 			}
-			self.chooseOpponent();
+			outerScope.chooseOpponent();
 		});
 
 	},
 	chooseOpponent : function(){
-		var self = this;
-		$(document).on('click', '.enemies', function(){
-			if (self.stage === "choose opponent"){
+		var outerScope = this;
+		$('.enemies').on('click', function(){
+			if (outerScope.stage === "choose opponent"){
 				$(this).appendTo('#ring').addClass('opponent');
-				console.log(this);
-				self.stage = "fight";
+				outerScope.stage = "fight";
+				window.selectedOpponent = $(this).attr('data-name');
+				console.log(selectedOpponent);
 			}
 		});
 	}, 
@@ -77,10 +73,9 @@ var game = {
 	reset : function(){
 		this.chooseCharacter();
 		$('.report').html("");
-		this.clickCounter = 0;
+		this.clickCounter = 1;
 		this.stage = "choose character";
 		$('.character').show().appendTo('#choose-character').removeClass('enemies you opponent');
-		console.log(this.stage);
 		this.regina.healthPoints = 150;
 		this.kathryn.healthPoints = 200;
 		this.taylor.healthPoints = 175;
@@ -90,46 +85,49 @@ var game = {
 		$("*[data-name = 'kathryn']").find('.health').html(this.kathryn.healthPoints);
 		$("*[data-name = 'taylor']").find('.health').html(this.taylor.healthPoints);
 		$("*[data-name = 'sharpay']").find('.health').html(this.sharpay.healthPoints);
+	},
+	updateCharacterHealth : function (){
+		this[selectedCharacter]['healthPoints']-= this[selectedOpponent]['counterAttackPower'];
+		$('.you').find('.health').html(this[selectedCharacter]['healthPoints']);
+	},
+	updateOpponentHealth : function (){
+		window.attackPower = this[selectedCharacter]['attackPower'] * this.clickCounter; 
+		this[selectedOpponent]['healthPoints']-= attackPower;
+		$('.opponent').find('.health').html(this[selectedOpponent]['healthPoints']); 
+	},
+	updateGameStatusReport : function (){
+	$('.report').html("You attacked " + this[selectedOpponent]['fullName'] + " for " + attackPower + " damage.<br/>" + this[selectedOpponent]['fullName'] + " attacked you back for " + this[selectedOpponent]['counterAttackPower']+ " damage.")
 	}
 }	
-	game.reset();
 
-	$(document).on('click', '.reset', function(){
-			$(this).hide();
-			game.reset();
-		});
+game.reset();
 
-	$('.attack').on('click', function(){
-		var characterName = $('.you').attr('data-name');
-		var opponentName = $('.opponent').attr('data-name');
-
-		//update character health points
-		game[characterName]['healthPoints']-= game[opponentName]['counterAttackPower'];
-		console.log("character healthPoints " + game[characterName]['healthPoints']);
-		$('.you').find('.health').html(game[characterName]['healthPoints']);
-
-		//attack calculation
-		game[characterName]['attackPower'] = game[characterName]['origAttackPower'] * game.clickCounter + game[characterName]['attackPower'] ;
-		console.log("character attackPower " + game[characterName]['attackPower']);
-
-		//update opponent health points
-		game[opponentName]['healthPoints']-= game[characterName]['attackPower'];
-		console.log("opponent health " + game[opponentName]['healthPoints']);
-		$('.opponent').find('.health').html(game[opponentName]['healthPoints']); 
-
-		//update game status report
-		$('.report').html("You attacked " + game[opponentName]['fullName'] + " for " + game[characterName]['attackPower'] + " damage.<br/>" + game[opponentName]['fullName'] + " attacked you back for " + game[opponentName]['counterAttackPower']+ " damage.")
-
-		 if(game[opponentName]['healthPoints'] <= 0){
-		 	game.winBattle();
-		 } else if (game[characterName]['healthPoints'] <= 0){
-		 	game.loseBattle();
-		 }
-		 game.clickCounter++
-		 console.log("Click Counter " + game.clickCounter);
-
+$('.reset').on('click', function(){
+		$(this).hide();
+		game.reset();
 	});
 
+$('.attack').on('click', function(){
+
+	game.updateCharacterHealth();
+	game.updateOpponentHealth();
+	game.updateGameStatusReport();
+
+	 if(game[selectedOpponent]['healthPoints'] <= 0){
+	 	game.winBattle();
+	 } else if (game[selectedCharacter]['healthPoints'] <= 0){
+	 	game.loseBattle();
+	 }
+	 game.clickCounter++
+
+});
+
+
+
+
+
+
+	
 
 
 
